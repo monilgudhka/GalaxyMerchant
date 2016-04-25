7c47f3c5ba3f81c11105505729d6dd5aa13da2ba
monil <monil@MONIL-PC>
2016-04-25 22:55:27 +0530

    packaging

--- src/com/monilgudhka/galaxymerchant/DataMap.java
+++ src/com/monilgudhka/galaxymerchant/DataMap.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package galaxymerchant;
+package com.monilgudhka.galaxymerchant;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/com/monilgudhka/galaxymerchant/Symbol.java
+++ src/com/monilgudhka/galaxymerchant/Symbol.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package galaxymerchant;
+package com.monilgudhka.galaxymerchant;
 
 /**
  * Symbol defines Roman Numeral and its decimal value
